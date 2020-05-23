6075414df1470a9357d317743a3629fc9ee399fa
Reid Netterville III <reidiiius@gmail.com>
2020-05-23 16:57:02 -0500

    1590271022

--- bandana.jl
+++ bandana.jl
@@ -8,101 +8,113 @@ include("paisley.jl")
 using .Paisley
 
 
-epoch = strftime("h%s", time())
+function entryway(args...)
 
-attune, layout = "guitar", eadgbe
+  local epoch = strftime("h%s", time())
+
+  local attune = "guitar"
+
+  local layout = eadgbe
 
 
-function sentinel(seal)
-  ismatch(r"^([jknz]\d+)+([lm]\d+)?([o-z]{2})?$", seal)
-end
+  function sentinel(seal::String)
+    ismatch(r"^([jknz]+\d+)+([lm]\d+)?[o-z]*$", seal)
+  end
 
 
-function fabric(seal="z0")
-  try
-    if sentinel(seal) && haskey(codex, seal)
-      diadem = "$attune-$seal-$epoch"
-      println("\n\t", diadem)
-      for course in layout(seal)
-        println("\t", course)
+  function fabric(seal::String="z0")
+    try
+      if sentinel(seal) && haskey(codex, seal)
+        local diadem = "$attune-$seal-$epoch"
+        println("\n\t", diadem)
+        for course in layout(seal)
+          println("\t", course)
+        end
+      else
+        println("\n\t", seal, '?')
+      end 
+    catch anomaly
+      println("\ncause $anomaly")
+    end
+  end
+
+
+  function atrium(cargo::Array)
+    for seal in cargo
+      fabric(seal)
+    end
+  end
+
+
+  local argots = copy(collect(args))
+
+  local dexter = length(argots)
+
+  if dexter > 0
+    local orchid = argots[1]
+
+    if dexter == 1
+      fabric(orchid)
+      println()
+
+    elseif dexter > 1
+
+      if orchid == "aug4th" ||
+         orchid == "a4" ||
+         orchid == "b5" ||
+         ismatch(r"^(bf)+b?$", orchid)
+        attune, layout = orchid, bfbfb
+        shift!(argots)
+
+      elseif orchid == "cello" ||
+             orchid == "p5" ||
+             ismatch(r"^.*gda.*$", orchid) ||
+             ismatch(r"^mando.*", orchid) ||
+             ismatch(r"^viol.*", orchid)
+        attune, layout = orchid, cgdae
+        shift!(argots)
+
+      elseif orchid == "gtr" ||
+             ismatch(r"^g.*it.*r$", orchid) ||
+             ismatch(r"^.*dgbe.*$", orchid) ||
+             ismatch(r"^.*gcea.*$", orchid) ||
+             ismatch(r"^uk[ue](le)*$", orchid)
+        attune, layout = orchid, eadgbe
+        shift!(argots)
+
+      elseif orchid == "fkbjdn" ||
+             orchid == "maj3rd" ||
+             orchid == "m3"
+        attune, layout = orchid, fkbjdn
+        shift!(argots)
+
+      elseif orchid == "bass" ||
+             orchid == "p4" ||
+             ismatch(r"^.*eadg.*$", orchid)
+        attune, layout = orchid, dragon
+        shift!(argots)
+
+      else
+        nothing
       end
-    else
-      println("\n\t", seal, '?')
-    end 
-  catch anomaly
-    println("\ncause $anomaly")
-  end
-end
 
-
-function atrium(cargo)
-  for seal in cargo
-    fabric(seal)
-  end
-  println()
-end
-
-
-argots = collect(ARGS)
-
-dexter = length(argots)
-
-if dexter > 0
-  orchid = argots[1]
-
-  if dexter == 1
-    fabric(orchid)
-    println()
-
-  elseif dexter > 1
-
-    if orchid == "aug4th" ||
-       orchid == "a4" ||
-       ismatch(r"^(bf)+b?$", orchid) ||
-       orchid == "b5"
-      attune, layout = orchid, bfbfb
-      shift!(argots)
-
-    elseif orchid == "cello" ||
-           ismatch(r"^.*gda.*$", orchid) ||
-           ismatch(r"^mando.*", orchid) ||
-           orchid == "p5" ||
-           ismatch(r"^viol.*", orchid)
-      attune, layout = orchid, cgdae
-      shift!(argots)
-
-    elseif ismatch(r"^.*dgbe.*$", orchid) ||
-           ismatch(r"^.*gcea.*$", orchid) ||
-           ismatch(r"^g.*it.*r$", orchid) ||
-           orchid == "gtr" ||
-           ismatch(r"^uk[ue](le)*$", orchid)
-      attune, layout = orchid, eadgbe
-      shift!(argots)
-
-    elseif orchid == "fkbjdn" ||
-           orchid == "maj3rd" ||
-           orchid == "m3"
-      attune, layout = orchid, fkbjdn
-      shift!(argots)
-
-    elseif orchid == "bass" ||
-           ismatch(r"^.*eadg.*$", orchid) ||
-           orchid == "p4"
-      attune, layout = orchid, dragon
-      shift!(argots)
-
-    else
-      nothing
+      local cargo = copy(argots)
+      atrium(cargo)
     end
 
-    cargo = collect(argots)
-    atrium(cargo)
+  else
+    catalog()
   end
 
-else
-  catalog()
-  println()
 end
+
+if length(ARGS) > 0
+  entryway(ARGS...)
+else
+  entryway()
+end
+
+println()
 
 
 end # module
