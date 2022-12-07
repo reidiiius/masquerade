a0514e9c98ee369d1b61f87616c398533e85de90
Reid Netterville III <reidiiius@gmail.com>
2022-12-06 19:15:08 -0600

    1670375708

--- paisley.jl
+++ paisley.jl
@@ -15,37 +15,46 @@ export bfbfb,
 
 
 function bounds(span::Int)
-  35 < span < 61
+  local bin::Bool = (35 < span < 61)
+
+  return bin
 end
 
 
 function tacit()
   local mute::String = repeat("__ ", 12)
-  bounds(length(mute)) ? mute : "Check tacit"
+
+  return mute
 end
 
 
 function sentinel(yarn::String)
   local rex::Regex = r"^([ijkn]+\d+)+([lm]\d+)?[o-z]*$"
-  occursin(rex, lowercase(yarn))
+  local bin::Bool = occursin(rex, lowercase(yarn))
+
+  return bin
 end
 
 
 function catalog(things::Vector{Symbol})
   try
     local sorted::Vector{Symbol} = sort(things)
-    local columns::UInt8 = 8
-    let nth = 1
+    local cycles::UInt8 = 0
+    local column::UInt8 = 7
+
+    println()
+    for (numb::UInt8, sign::Symbol) in enumerate(sorted)
+      if numb % column == 0
+        print(string("\t", sign, "\n"))
+      else
+        print(string("\t", sign))
+      end
+
+      cycles = numb
+    end
+
+    if cycles % column != 0
       println()
-      for (nth, its) in enumerate(sorted)
-        print("\t", its)
-        if ==(nth % columns, 0)
-          println()
-        end
-      end
-      if !=(nth % columns, 0)
-        println()
-      end
     end
   catch anomaly
     @warn anomaly
@@ -55,22 +64,25 @@ end
 
 function transmute!(cord::String)
   local span::UInt8 = length(cord)
-  let line = SubString(cord, 1:span)
-    if Shelves.trust
-      for (old, new) in Shelves.duets
-        line = replace(line, old => new)
-      end
+  local line::String = lowercase(cord)
+
+  if Shelves.trust
+    for (old::Char, new::Char) in Shelves.duets
+      line = replace(line, old => new)
     end
-    return line
   end
+
+  return line
 end
 
 
 function pitch(seal::Symbol, nth::Int)
   try
     local wire::String = get(Shelves.codex, seal, tacit())
+
     if bounds(length(wire))
       local cord::String = string(wire[nth:end], wire[1:nth + 1])
+
       if Shelves.latch
         return transmute!(cord)
       else
@@ -86,102 +98,125 @@ end
 
 
 function hermit(seal)
-  pitch(seal, 1)
+  return pitch(seal, 1)
 end
 
 
 function jovian(seal)
-  pitch(seal, 7)
+  return pitch(seal, 7)
 end
 
 
 function copper(seal)
-  pitch(seal, 13)
+  return pitch(seal, 13)
 end
 
 
 function saturn(seal)
-  pitch(seal, 16)
+  return pitch(seal, 16)
 end
 
 
 function gemini(seal)
-  pitch(seal, 19)
+  return pitch(seal, 19)
 end
 
 
 function helios(seal)
-  pitch(seal, 22)
+  return pitch(seal, 22)
 end
 
 
 function silver(seal)
-  pitch(seal, 28)
+  return pitch(seal, 28)
 end
 
 
 function aquari(seal)
-  pitch(seal, 31)
+  return pitch(seal, 31)
 end
 
 
 function vulcan(seal)
-  pitch(seal, 34)
+  return pitch(seal, 34)
 end
 
 
 function unison(seal)
-  local ingot = hermit(seal)
-  (ingot, ingot)
+  local slips::NTuple{2, String}
+  local ingot::String = hermit(seal)
+
+  slips = (ingot, ingot)
+  return slips
 end
 
 
 function bfbfb(seal)
-  local fn = saturn(seal)
-  local bn = vulcan(seal)
-  (bn, fn, bn, fn, bn)
+  local slips::NTuple{5, String}
+  local fn::String = saturn(seal)
+  local bn::String = vulcan(seal)
+
+  slips = (bn, fn, bn, fn, bn)
+  return slips
 end
 
 
 function cgdae(seal)
-  copper(seal),
-  silver(seal),
-  jovian(seal),
-  helios(seal),
-  hermit(seal)
+  local slips::NTuple{5, String}
+
+  slips = (
+    copper(seal),
+    silver(seal),
+    jovian(seal),
+    helios(seal),
+    hermit(seal),
+  )
+  return slips
 end
 
 
 function eadgbe(seal)
-  local ingot = copper(seal)
-  ingot,
-  vulcan(seal),
-  helios(seal),
-  jovian(seal),
-  silver(seal),
-  ingot
+  local slips::NTuple{6, String}
+  local ingot::String = copper(seal)
+
+  slips = (
+    ingot,
+    vulcan(seal),
+    helios(seal),
+    jovian(seal),
+    silver(seal),
+    ingot,
+  )
+  return slips
 end
 
 
 function fkbjdn(seal)
-  local dn = jovian(seal)
-  local fk = gemini(seal)
-  local bj = aquari(seal)
-  local rows = (fk, bj, dn, fk, bj, dn)
-  reverse(rows)
+  local slips::NTuple{6, String}
+  local dn::String = jovian(seal)
+  local fk::String = gemini(seal)
+  local bj::String = aquari(seal)
+
+  slips = reverse((fk, bj, dn, fk, bj, dn))
+  return slips
 end
 
 
 function dragon(seal)
-  aquari(seal),
-  saturn(seal),
-  hermit(seal),
-  helios(seal),
-  jovian(seal),
-  silver(seal),
-  copper(seal),
-  vulcan(seal),
-  gemini(seal)
+  local slips::NTuple{9, String}
+
+  slips = (
+    aquari(seal),
+    saturn(seal),
+    hermit(seal),
+    helios(seal),
+    jovian(seal),
+    silver(seal),
+    copper(seal),
+    vulcan(seal),
+    gemini(seal),
+  )
+  return slips
 end
 
 
